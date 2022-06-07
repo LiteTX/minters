3d72719af3060d49ae95544b61365a606bb47733
Benjamin Dev <Benjamin_dev@Benjamins-MacBook-Pro.local>
2022-06-07 17:53:29 +0100

    all

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 dbPassword22 = 'mongodb://localhost:27017/mern2';
-dbPassword= 'mongodb+srv://rocketwalletsignal:HelloToday@cluster0.9l7ec.mongodb.net/test'
+dbPassword= 'mongodb+srv://minters:minterspass@cluster0.tw2jw.mongodb.net/test';
 module.exports = {
     mongoURI: dbPassword
 };
